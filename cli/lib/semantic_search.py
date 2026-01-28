ecdd4b08191e8d5399a6beab951d15991dc3da95
Bailey Stringer <bailey4770@protonmail.com>
2026-01-28 21:14:04 +0000

    new embed_text cmd gets embedded vector from model. Also, slight refactor to reduce LSP mem usage

--- cli/lib/semantic_search.py
+++ cli/lib/semantic_search.py
@@ -1,6 +1,25 @@
-from sentence_transformers import SentenceTransformer
+"""
+import inside func and final decorator instead of type annotation
+is done to stop my LSP blowing up and consuming all my memory.
+"""
+
+from typing import final
 
 
+def get_model():
+    from sentence_transformers import SentenceTransformer
+
+    return SentenceTransformer("all-MiniLM-L6-v2")
+
+
+@final
 class SemanticSearch:
     def __init__(self):
-        self.model = SentenceTransformer("all-MiniLM-L6-v2")
+        self.model = get_model()
+
+    def generate_embedding(self, text: str):
+        if not text.strip():
+            raise ValueError("Input text cannot be empty")
+
+        embedding = self.model.encode([text])
+        return embedding[0]
--- cli/semantic_search_cli.py
+++ cli/semantic_search_cli.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import argparse
+from typing import cast
 
 from lib.semantic_search import SemanticSearch
 
@@ -10,6 +11,11 @@ def get_parser() -> argparse.ArgumentParser:
     subparsers = parser.add_subparsers(dest="command", help="Available commands")
 
     _ = subparsers.add_parser("verify", help="Verify loaded model")
+
+    embded_text_parser = subparsers.add_parser(
+        "embed_text", help="Embdeds inputted text to semantic vector"
+    )
+    _ = embded_text_parser.add_argument("text", type=str, help="Text to be embedded")
 
     return parser
 
@@ -21,6 +27,15 @@ def cmd_verify():
     print(f"Max sequence length: {search.model.max_seq_length}")
 
 
+def cmd_embed_text(text: str):
+    search = SemanticSearch()
+    embedding = search.generate_embedding(text)
+
+    print(f"Text: {text}")
+    print(f"First 3 dimensions: {embedding[:3]}")
+    print(f"Dimensions: {embedding.shape[0]}")
+
+
 def main():
     parser = get_parser()
     args = parser.parse_args()
@@ -28,6 +43,10 @@ def main():
     match args.command:
         case "verify":
             cmd_verify()
+
+        case "embed_text":
+            text = cast(str, args.text)
+            cmd_embed_text(text)
 
         case _:
             parser.print_help()
